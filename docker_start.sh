541c84d7e19c227e990a17d3f24e75b9c8ac7a03
patrick.ekkel <patrick.ekkel@topicus.nl>
2015-12-04 10:56:14 +0100

    docker start script uitgebreid

--- docker_start.sh
+++ docker_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-DOCKER_ID=$1
-docker run -d -e JAVA_HOME=JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -ti $1 bash
+# start docker container
+CONTAINER_ID=$(docker run -d -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -ti $1  bash)
+echo "started container with ID" $CONTAINER_ID
+# run commands to get the container ready for usage
+docker exec $CONTAINER_ID /root/docker_startup_services.sh
+docker exec $CONTAINER_ID /root/build_and_deploy.sh
+#docker stop $CONTAINER_ID
+ 
