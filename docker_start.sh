67e51185c392cbabece8699ad4763fb894dfe224
patrick.ekkel <patrick.ekkel@topicus.nl>
2015-12-08 16:21:34 +0100

    per ongeluk iets uitgecommit

--- docker_start.sh
+++ docker_start.sh
@@ -5,7 +5,7 @@ JENKINS_HOME=$2
 CONTAINER_ID=$(docker run -d -p 9001:8090 -i -t -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -v $2:/top_build/ $1 bash)
 echo "started container with ID" $CONTAINER_ID
 # run commands to get the container ready for usage
-#docker exec $CONTAINER_ID /root/docker_startup_services.sh
-#docker exec $CONTAINER_ID /root/build_and_deploy.sh
+docker exec $CONTAINER_ID /root/docker_startup_services.sh
+docker exec $CONTAINER_ID /root/build_and_deploy.sh
 #docker stop $CONTAINER_ID
  
