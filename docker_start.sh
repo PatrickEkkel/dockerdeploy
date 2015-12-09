a97f67b104792b8914b8333909166c3c72e86eb3
patrick.ekkel <patrick.ekkel@topicus.nl>
2015-12-09 16:56:14 +0100

    je kan nu de branch opgeven die je wilt bouwen

--- docker_start.sh
+++ docker_start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # start docker container
 JENKINS_HOME=$2
+BRANCH_TO_BUILD=$3
+DOCKER_IMAGE=$1
 #CONTAINER_ID=$(docker run -d -v /top_build/ $2 -p 8090:9001 -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -ti $1  bash)
-CONTAINER_ID=$(docker run -d -p 9001:8090 -i -t -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -v $2:/top_build/ $1 bash)
+CONTAINER_ID=$(docker run -d -p 9001:8090 -i -t -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -v $2:/top_build/ $DOCKER_IMAGE bash)
 echo "started container with ID" $CONTAINER_ID
 # run commands to get the container ready for usage
+
 docker exec $CONTAINER_ID /root/docker_startup_services.sh
-docker exec $CONTAINER_ID /root/build_and_deploy.sh
+docker exec $CONTAINER_ID /root/build_and_deploy.sh $BRANCH_TO_BUILD
 #docker stop $CONTAINER_ID
  
