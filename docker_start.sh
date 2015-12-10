c217636be0e34a9d31e5af2a0fc804513d379125
patrick.ekkel <patrick.ekkel@topicus.nl>
2015-12-10 16:31:14 +0100

    poort waar op de host naar geluisterd wordt is nu instelbaar

--- docker_start.sh
+++ docker_start.sh
@@ -3,8 +3,9 @@
 JENKINS_HOME=$2
 BRANCH_TO_BUILD=$3
 DOCKER_IMAGE=$1
+PORT=$4
 #CONTAINER_ID=$(docker run -d -v /top_build/ $2 -p 8090:9001 -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -ti $1  bash)
-CONTAINER_ID=$(docker run -d -p 9001:8090 -i -t -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -v $2:/top_build/ $DOCKER_IMAGE bash)
+CONTAINER_ID=$(docker run -d -p $PORT:8090 -i -t -e JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64 -v $2:/top_build/ $DOCKER_IMAGE bash)
 echo "started container with ID" $CONTAINER_ID
 # run commands to get the container ready for usage
 
